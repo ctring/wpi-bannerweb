4f1329b1865136a506c83a9b1cf0365227f57ce4
Cuong ND <nguyentricuong1995@gmail.com>
2015-05-09 03:47:27 -0400

    Start working on log in

---
+++ app/src/main/java/com/cuongnd/wpibannerweb/SingleFragmentActivity.java
@@ -0,0 +1,29 @@
+package com.cuongnd.wpibannerweb;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+
+public abstract class SingleFragmentActivity extends Activity {
+    protected abstract Fragment createFragment();
+
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        FragmentManager manager = getFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            manager.beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit();
+        }
+    }
+}
