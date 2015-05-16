75e5655fbca48c3297a5b39b386c3791316dde10
Cuong ND <nguyentricuong1995@gmail.com>
2015-05-16 01:57:00 -0400

    Start working on Final Grade Page

--- app/src/main/java/com/cuongnd/wpibannerweb/grade/FinalGradePage.java
+++ app/src/main/java/com/cuongnd/wpibannerweb/grade/FinalGradePage.java
@@ -1,8 +1,16 @@
 package com.cuongnd.wpibannerweb.grade;
 
+import android.content.Context;
+
 /**
  * Created by Cuong Nguyen on 5/13/2015.
  */
 public class FinalGradePage {
+    Context mContext;
+
+    public FinalGradePage(Context context) {
+        mContext = context;
+    }
+
 
 }
